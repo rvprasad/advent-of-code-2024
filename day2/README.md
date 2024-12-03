60c091ab6b38552e8dce491ad38a2a4fed8602e4
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-03 11:57:03 -0600

    Solve the second part of day 2 puzzle

--- day2/README.md
+++ day2/README.md
@@ -2,7 +2,11 @@ Execute this code via `crystal day2.cr <filename>`
 
 ### Observations
 
-1. Crystal documentation is good.
-2. Crystal does not seem to have method parity with Ruby.  For example, while
-   Ruby supports `drop`, `drop_while`, `take`, and `take_while` method in
-   `Array`, Crystal only supports `skip`, `skip_while`, and `take_while`.
+1. Crystal's documentation is good.
+2. Crystal's standard library does not seem to have method parity with Ruby's
+   standard library.  For example, while Ruby's library supports `drop`,
+   `drop_while`, `take`, and `take_while` method in `Array`, Crystal's library
+   only supports `skip`, `skip_while`, and `take_while`.  
+3. Even with prior exposure to Ruby's syntax and concepts via Groovy, the
+   experience with Crystal involved some friction; possibly, due to
+   observation 2 above.
--- day2/day2.cr
+++ day2/day2.cr
@@ -1,22 +1,68 @@
 def getReports(filename)
   File.read_lines(filename).map { |line|
-    line.split().map { |i| i.to_i }
+    line.split.map { |i| i.to_i }
   }
 end
 
 def zipWithNext(list)
-  (list.first (list.size - 1)).zip(list.skip(1))
+  (list.first(list.size - 1)).zip(list.skip(1))
 end
 
-def isSafe(r) 
-    diffs = zipWithNext(r).map { |a, b| a - b }
-    diffs.all? { |d| 1 <= d.abs <= 3 } &&
-      (diffs.all? { |d| d > 0 } || diffs.all? { |d| d < 0 })
+def safe?(r)
+  diffs = zipWithNext(r).map { |a, b| a - b }
+  diffs.all? { |d| 1 <= d.abs <= 3 } &&
+    (diffs.all? { |d| d > 0 } || diffs.all? { |d| d < 0 })
 end
 
 def countSafeReports(reports)
-  reports.select { |r| isSafe(r) }.size()
+  reports.select { |r| safe?(r) }.size
+end
+
+def intolerable?(diffs)
+  zeroes = diffs.select { |d| d == 0 }.size
+  positives = diffs.select { |d| d > 0 }.size
+  negatives = diffs.select { |d| d < 0 }.size
+  big_diffs = diffs.select { |d| d.abs > 3 }.size
+  zeroes > 1 ||                      # more than one zero diff cannot be fixed
+    big_diffs > 2 ||                 # more than two big diffs cannot be fixed; two consecutive big diffs may be fixed
+    (big_diffs > 0 && zeroes > 0) || # a zero diff and a big diff cannot be fixed
+    (positives > 1 && negatives > 1) # more than one positive diff and one negative diff cannot be fixed
+end
+
+def safeAfterDeletion?(r, idx)
+  safe?(r.dup.tap { |x| x.delete_at(idx) })
+end
+
+def tolerablySafe?(r)
+  diffs = zipWithNext(r).map { |a, b| a - b }
+  if intolerable?(diffs)
+    return false
+  end
+
+  zero_index = diffs.index { |e| e == 0 }
+  if zero_index
+    return safeAfterDeletion?(r, zero_index)
+  end
+
+  big_diff_index = diffs.index { |e| e.abs > 3 }
+  if big_diff_index
+    return safeAfterDeletion?(r, big_diff_index) || safeAfterDeletion?(r, big_diff_index + 1)
+  end
+
+  out_of_order_index = zipWithNext(diffs).index { |(a, b)| a.positive? ^ b.positive? }
+  if out_of_order_index
+    return safeAfterDeletion?(r, out_of_order_index) ||
+      safeAfterDeletion?(r, out_of_order_index + 1) ||
+      safeAfterDeletion?(r, out_of_order_index + 2)
+  end
+
+  raise "A report slipped through our check!! #{r}"
+end
+
+def countTolerablySafeReports(reports)
+  reports.select { |r| safe?(r) || tolerablySafe?(r) }.size
 end
 
 reports = getReports(ARGV[0])
 puts "Number of safe reports #{countSafeReports(reports)}"
+puts "Number of tolerable safe reports #{countTolerablySafeReports(reports)}"
