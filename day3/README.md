9da395b3ca12ab150370673ac05bcb41f03f476a
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-03 23:09:42 -0600

    Solve the second part of day 3 puzzle

--- day3/README.md
+++ day3/README.md
@@ -2,4 +2,4 @@ Execute this code via `elixir day3.exs <filename>`.
 
 ### Observations
 
-1. Getting back into Elixir was easy.  Just like with Clojure :)
+1. Getting back into Elixir was easy :)
--- day3/day3.exs
+++ day3/day3.exs
@@ -1,21 +1,35 @@
 defmodule Day3 do
   def get_memory(filename) do
-    File.stream!(filename) |> Stream.map(&Function.identity/1) 
-  end 
+    File.stream!(filename) 
+    |> Stream.map(&String.trim_trailing/1) 
+    |> Enum.join("")
+  end
 
   def get_multiplicands1(line) do
-    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, line) |> Enum.map(&Enum.drop(&1, 1))
+    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, line) 
+    |> Enum.map(&Enum.drop(&1, 1))
+  end
+
+  def get_multiplicands2(line) do
+    Regex.scan(~r/do\(\)(.*?)(don't\(\)|$)/, "do()" <> line)
+    |> Enum.map(&Enum.at(&1, 1))
+    |> Enum.join("")
+    |> then(&Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, &1))
+    |> Enum.map(&Enum.drop(&1, 1))
   end
 
   def get_result(filename, get_multiplicands) do
     get_memory(filename)
-    |> Enum.map(get_multiplicands)
-    |> Enum.flat_map(&Function.identity/1)
-    |> Enum.map(fn pair -> Enum.map(pair, &Integer.parse(&1)) |> Enum.map(&elem(&1, 0)) end)
+    |> get_multiplicands.()
+    |> Enum.map(fn pair ->
+      Enum.map(pair, &Integer.parse(&1))
+      |> Enum.map(&elem(&1, 0))
+    end)
     |> Enum.map(&Enum.product/1)
     |> Enum.sum()
   end
 end
 
 filename = System.argv() |> List.first()
-Day3.get_result(filename, &Day3.get_multiplicands1/1) |> IO.puts
+Day3.get_result(filename, &Day3.get_multiplicands1/1) |> IO.puts()
+Day3.get_result(filename, &Day3.get_multiplicands2/1) |> IO.puts()
