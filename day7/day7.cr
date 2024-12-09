6ab953b57ff488ae8af24830295ba293b6072dec
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-09 13:12:48 -0600

    Solve the second part of day 7 puzzle

--- day7/day7.cr
+++ day7/day7.cr
@@ -5,26 +5,41 @@ def getEquations(filename)
   }
 end
 
-def isValid?(test_value, values, curr_value)
+def isValidPart1?(test_value, values, curr_value)
   if values.empty?
     curr_value == test_value
   elsif curr_value <= test_value
     head, *tail = values
-    isValid?(test_value, tail, curr_value * head) ||
-      isValid?(test_value, tail, curr_value + head)
+    isValidPart1?(test_value, tail, curr_value * head) ||
+      isValidPart1?(test_value, tail, curr_value + head)
+  else
+    false
+  end
+end
+
+def isValidPart2?(test_value, values, curr_value)
+  if values.empty?
+    curr_value == test_value
+  elsif curr_value <= test_value
+    head, *tail = values
+    isValidPart2?(test_value, tail, curr_value * head) ||
+      isValidPart2?(test_value, tail, curr_value + head) ||
+      isValidPart2?(test_value, tail, "#{curr_value}#{head}".to_u64)
   else
     false
   end
 end
 
 def solve(equations)
-  part1 = equations.select { |k, v| isValid?(k, v[1..], v[0]) }
+  # Crystal bug results in Arithmetic overflow error
+  # part1 = equations.map { |k, v| isValidPart1?(k, v[1..], v[0]) ? k : 0 }.sum
+
+  part1 = equations.select { |k, v| isValidPart1?(k, v[1..], v[0]) }
     .map { |k, _| k }.sum
 
-  # Crystal bug results in Arithmetic overflow error
-  # part1 = equations.map { |k, v| isValid?(k, v[1..], v[0]) ? k : 0 }.sum
-
-  {part1}
+  part2 = equations.select { |k, v| isValidPart2?(k, v[1..], v[0]) }
+    .map { |k, _| k }.sum
+  {part1, part2}
 end
 
 puts solve(getEquations(ARGV[0]))
