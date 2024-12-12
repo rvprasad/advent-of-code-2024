145439a644b36c55eb135c9b7119094158cb0ac4
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-11 23:09:01 -0600

    Solve second part of day 11 puzzle

--- day11/src/day11.clj
+++ day11/src/day11.clj
@@ -21,6 +21,29 @@
     stones
     (recur (apply concat (map expand stones)) (- n 1))))
 
+(defn make-stone-level [stone n] (str stone '-' n))
+
+(defn count-stones [stone n stone-level2count]
+  (let [stone-level (make-stone-level stone  n)]
+    (if (some? (stone-level2count stone-level))
+      stone-level2count
+      (if (= n 0)
+        (assoc stone-level2count stone-level 1)
+        (let [next-stones (expand stone)
+              next-level (- n 1)
+              next-stone-levels (map #(make-stone-level % next-level) next-stones)
+              new-stone-level2count (reduce #(count-stones %2 next-level %1)
+                                            stone-level2count
+                                            next-stones)
+              counts (map new-stone-level2count next-stone-levels)]
+          (assoc new-stone-level2count stone-level (reduce + counts)))))))
+
+(defn solve-part-2 [stones n]
+  (let [stone-level2count (reduce #(count-stones %2 n %1) {} stones)
+        stone-levels (map #(make-stone-level % n) stones)]
+    (reduce + (map #(stone-level2count %) stone-levels))))
+
 (defn -main [filename]
   (let [stones (read-stone-list filename)]
-    (println (count (solve-part-1 stones 25)))))
+    (println (count (solve-part-1 stones 25)))
+    (println (solve-part-2 stones 75))))
