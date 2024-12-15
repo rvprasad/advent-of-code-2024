0a62d8501af7537e9d2510691c51ae5b1ef3eaa5
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-15 17:02:29 -0600

    Speed up the solution for day 6 puzzle

--- day6/src/day6.clj
+++ day6/src/day6.clj
@@ -31,44 +31,54 @@
 
 (defn is-looping? [seen pos] (contains? seen pos))
 
-(defn is-blocked? [area-map loc] (not (contains? area-map loc)))
+(defn is-out-of-area? [area-map loc] (not (contains? area-map loc)))
 
-(defn traverse-path [init-path area-map]
+(defn traverse-path [path area-map]
   (defn helper [path seen]
     (let [curr-pos (first path)
-          curr-loc (first curr-pos)
-          curr-content (area-map curr-loc)
-          next-pos (get-next-pos curr-pos area-map)
-          next-loc (first next-pos)]
+          next-pos (get-next-pos curr-pos area-map)]
       (cond
         (is-looping? seen next-pos) nil
-        (is-blocked? area-map next-loc) path
+        (is-out-of-area? area-map (first next-pos)) path
         :else (recur (cons next-pos path)
                      (conj seen next-pos)))))
-  (helper init-path (set nil)))
+  (helper path (set (rest path))))
 
-(defn is-map-loopy-with-obstacle [area-map obs-loc init-path]
-  (let [new-map (assoc area-map obs-loc "#")]
-    (traverse-path init-path new-map)))
+(defn obs-not-in-path? [obs-loc path]
+  (->>
+   [:up :down :right :left]
+   (map #(vector obs-loc %))
+   (not-any? (set path))))
+
+(defn is-path-loopy-with-obstacle-at-head? [path area-map]
+  (let [obs-loc (first (first path))
+        rest-path (rest path)]
+    (if (and (not= (area-map obs-loc) "^")
+             (obs-not-in-path? obs-loc rest-path))
+      (let [final-path (traverse-path rest-path (assoc area-map obs-loc "#"))]
+        (if (nil? final-path) obs-loc)))))
 
 (defn solve-puzzle [area-map]
   (def max-cols (apply max (map #(second (first %)) area-map)))
   (def max-rows (apply max (map #(first (first %)) area-map)))
 
   (let [start-loc (first (first (filter #(= (second %) "^") area-map)))
-        init-path (list [start-loc :up])
-        path (traverse-path init-path area-map)
+        path (traverse-path (list [start-loc :up]) area-map)
         path-length (count (distinct (map first path)))
         loop-count (->>
                     path
-                    (map #(first %))
+                    (reverse)
+                    (reductions #(cons %2 %1) ())
+                    (filter #(> (count %) 1))
+                    (map #(is-path-loopy-with-obstacle-at-head? % area-map))
+                    (filter some?)
                     (distinct)
-                    (filter #(and (not= start-loc %)))
-                    (map #(is-map-loopy-with-obstacle area-map %1 init-path))
-                    (filter nil?)
                     (count))]
     [path-length loop-count]))
 
 (defn -main [filename]
   (let [area-map (read-area-map filename)]
     (println (solve-puzzle area-map))))
+
+
+
