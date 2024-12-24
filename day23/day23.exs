66607b0d7d9bb693b7fdc57e558f0201b2305604
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2024-12-24 17:41:39 -0600

    Solve the second part of the day 23 puzzle

--- day23/day23.exs
+++ day23/day23.exs
@@ -16,6 +16,7 @@ defmodule Day23 do
         |> Enum.flat_map(&Function.identity/1)
         |> Enum.uniq()
         |> Enum.filter(&(&1 != n))
+        |> MapSet.new()
 
       {n, neighbors}
     end)
@@ -36,11 +37,11 @@ defmodule Day23 do
     nodes |> Enum.all?(&(&1 in neighbors))
   end
 
-  def solve1(network) do
+  def get_3_clique(network) do
     Map.keys(network)
-    |> Enum.filter(&String.starts_with?(&1, "t"))
     |> Enum.map(fn n ->
       Map.get(network, n)
+      |> MapSet.to_list()
       |> combinations(2)
       |> Enum.filter(fn [a | [b]] ->
         is_neighbor_of(a, [n, b], network) && is_neighbor_of(b, [n, a], network)
@@ -49,10 +50,56 @@ defmodule Day23 do
     end)
     |> Enum.flat_map(&Function.identity/1)
     |> Enum.uniq()
+  end
+
+  def solve1(network) do
+    get_3_clique(network)
+    |> Enum.filter(fn c -> Enum.any?(c, &String.starts_with?(&1, "t")) end)
     |> Enum.count()
+  end
+
+  def grow_cliques(clique, network, seen_cliques) do
+    common_neighbors =
+      clique
+      |> Enum.reduce(nil, fn c, acc ->
+        if acc == nil do
+          Map.get(network, c)
+        else
+          MapSet.intersection(acc, Map.get(network, c))
+        end
+      end)
+      |> MapSet.difference(clique)
+
+    if Enum.empty?(common_neighbors) do
+      seen_cliques
+    else
+      new_cliques =
+        common_neighbors
+        |> Enum.map(&MapSet.put(clique, &1))
+        |> Enum.filter(&(!MapSet.member?(seen_cliques, &1)))
+        |> MapSet.new()
+
+      new_seen_cliques = MapSet.union(seen_cliques, new_cliques)
+
+      new_cliques
+      |> Enum.reduce(new_seen_cliques, fn cl, acc ->
+        grow_cliques(cl, network, acc)
+      end)
+    end
+  end
+
+  def solve2(network) do
+    get_3_clique(network)
+    |> Enum.reduce(MapSet.new(), fn cl, cliques ->
+      grow_cliques(MapSet.new(cl), network, cliques)
+    end)
+    |> Enum.max_by(&Enum.count/1)
+    |> Enum.sort()
+    |> Enum.join(",")
   end
 end
 
 args = System.argv()
 network = Day23.read_network(Enum.fetch!(args, 0))
 Day23.solve1(network) |> IO.puts()
+Day23.solve2(network) |> IO.puts()
